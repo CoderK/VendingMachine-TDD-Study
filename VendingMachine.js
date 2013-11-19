4c2a49b69c7694aada8622f44b0cfc89f624bc81
CoderK <jeokrang@hanmail.net>
2013-11-19 22:39:26 +0900

    second test code

--- VendingMachine.js
+++ VendingMachine.js
@@ -1,25 +1,9 @@
-function VendingMachine() {
-	this._priceChart = {
-		"Coke" : 50,
-		"Coffee" : 200		
-	}
+function VendingMachine() {};
 
-	this._coin = 0;
-};
+VendingMachine.prototype = {
 
-VendingMachine.prototype.buy = function(beverageName) {
-	if( this._priceChart[beverageName] > this._coin ) return null;
-
-	if (this._stock[beverageName] == 0) return null;
-	this._stock[beverageName]--;
-
-	return beverageName;
+    buy : function(sBeverage){
+        return sBeverage;
+    }
 }
 
-VendingMachine.prototype.supply = function(htSupply) {
-	this._stock = htSupply;	
-}
-
-VendingMachine.prototype.putCoin = function(inputCoin) {
-	this._coin = inputCoin;	
-}
