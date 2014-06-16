6544f02ec4c30b9892e1e9813c22228aea8fd4f9
CoderK <jeokrang@hanmail.net>
2014-06-16 22:00:36 +0900

    ready for chapter4

--- VendingMachine.js
+++ VendingMachine.js
@@ -21,10 +21,6 @@ VendingMachine.prototype = {
         this._htStocks = htStocks;
     },
 
-    getStocks : function(){
-        return this._htStocks;
-    },
-
     insertCoin : function(nCoin){
         this._nBalance += nCoin;
     },
@@ -35,10 +31,6 @@ VendingMachine.prototype = {
 
     setPrice : function(htPriceTable){
         this._htPriceTable = htPriceTable;
-    },
-
-    getPrice : function(){
-        return this._htPriceTable;
     },
 
     _canNotBuyIt: function (sBeverage) {
@@ -59,42 +51,7 @@ VendingMachine.prototype = {
 
     _hasNoStocks: function (sBeverage) {
         return !this._htStocks[sBeverage] || this._htStocks[sBeverage] < 1;
-    },
-
-    allBeverageInfo : function(){
-        var aoResults = Object.keys(this._htStocks).map(function(name){
-            return { name : name, stocks : this._htStocks[name], price : this._htPriceTable[name] };
-        }, this);
-
-        return aoResults;
     }
+
 };
-
-function VendingMachineView(htConfig) {
-    this._oVendingMachine = htConfig.model;
-    this._welBeveragePanel = $( htConfig.selector );
-    // this._welBeverageBtns = this._welBeveragePanel.find("li");
-
-    this._welBeveragePanel.click($.proxy(this._onClickBeverage, this));
-
-    var htStocks = this._oVendingMachine.getStocks();
-
-    var tpl = '<li class="item{INDEX} beverage" data-beverage-name="{NAME}"><button><span class="blind">{NAME}</span></button><span>{PRICE}</span></li>';
-
-    var sHtml = Object.keys(htStocks).map(function(name, i) {
-        return tpl.replace("{INDEX}", i).replace("{NAME}", name);
-    });
-
-    console.log(sHtml.join(""));
-
-    this._welBeveragePanel.find("ul").html( sHtml.join("") );
-};
-
-VendingMachineView.prototype = {
-    _onClickBeverage : function(welEvent){
-        var sBevName = $(welEvent.target).data("beverage-name");
-        this._oVendingMachine.buy(sBevName);
-
-        // this._oVendingMachine.buy($(welEvent.target).find("button > span").text());
-    }
-}
+ 
--- VendingMachine.qunit.js
+++ VendingMachine.qunit.js
@@ -1,4 +1,3 @@
-
 $(function() {
 
     var oVendingMachine = null;
